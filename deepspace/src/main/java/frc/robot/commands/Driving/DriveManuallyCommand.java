f6eee37d18257903327ab056892faa1b8ecfc51d
FYREStation <fyre5480@gmail.com>
2019-01-30 20:15:05 -0800

    Merge Conflicts Explained

--- deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
@@ -32,19 +32,19 @@ public class DriveManuallyCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-<<<<<<< HEAD:deepspace/src/main/java/frc/robot/commands/DriveManuallyCommand.java
-    double throttle = 0.25;  
-    double move = -Robot.m_oi.stick.getY() * throttle;
-    double turn = Robot.m_oi.stick.getX() * throttle;
-=======
-    
+
     /* double throttle = Robot.m_oi.stick.getThrottleChannel();
     double move = -Robot.m_oi.stick.getY()*throttle;
     double turn = Robot.m_oi.stick.getX(); */
+    
+    // double throttle = 0.25;  
+    // double move = -Robot.m_oi.stick.getY() * throttle;
+    // double turn = Robot.m_oi.stick.getX() * throttle;
+    
+  
 
     double move = Robot.m_oi.m_xBox.getY(Hand.kLeft) * 0.5;
     double turn = Robot.m_oi.m_xBox.getY(Hand.kRight) * 0.5;
->>>>>>> 52df3cc7f7b9c15e07c4727fa920971c872afe77:deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
     Robot.driveSubsystemRef.manualDrive(move, turn);
   }
 
