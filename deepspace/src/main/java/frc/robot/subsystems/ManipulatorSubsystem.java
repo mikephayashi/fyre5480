2f4cfc9023e81cf04cfec3be76db7f46520343a0
Parminder <parmarp1975@gmail.com>
2019-02-03 14:14:20 -0800

    rack and pinion motors out

--- deepspace/src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ deepspace/src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -40,7 +40,7 @@ public class ManipulatorSubsystem extends Subsystem {
   public Solenoid cargo_solenoid = new Solenoid(RobotMap.cargo_solenoid_port);
   public Solenoid hatch_solenoid = new Solenoid(RobotMap.hatch_solenoid_port);
   //Motors
-  Spark rackAndPinionMotorController = new Spark(RobotMap.rackAndPinionMotorController);
+  //Spark rackAndPinionMotorController = new Spark(RobotMap.rackAndPinionMotorController);
   //Spark cargoWingsMotorController = new Spark(RobotMap.cargo_wings_motor_port);
   //Encoders
   // public Encoder cargo_encoder = new Encoder(RobotMap.cargo_encoder_port_one, RobotMap.cargo_encoder_port_two, false, Encoder.EncodingType.k4X);
@@ -127,7 +127,7 @@ public class ManipulatorSubsystem extends Subsystem {
 
   //Extend Rack and pinion
   public void extendRackAndPinion(){
-    rackAndPinionMotorController.set(0.5);
+    //rackAndPinionMotorController.set(0.5);
   }
 
 
