b4b786e8214b447a1e6f863316eaf799e620046b
Parminder <parmarp1975@gmail.com>
2019-02-10 17:07:25 -0800

    set joystick to manual lift command

--- deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftCommand.java
@@ -25,8 +25,8 @@ public class ManualLiftCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    //double y_stick = something
-    //Robot.liftSubsystemRef.lift(stick);
+    double move = Robot.m_oi.stick.getY();
+    Robot.liftSubsystemRef.lift(move);
   }
 
   // Make this return true when this Command no longer needs to run execute()
