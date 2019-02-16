a4a25f6a0ee95da1082c0b51105e566216a4b293
Michael Hayashi <mikephayashi@gmail.com>
2019-02-15 20:08:32 -0800

    drive end set to 0

--- deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
@@ -25,7 +25,6 @@ public class DriveManuallyCommand extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.driveSubsystemRef.manualDrive(0, 0);
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -47,7 +46,7 @@ public class DriveManuallyCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    
+    Robot.driveSubsystemRef.manualDrive(0, 0);
   }
 
   // Called when another command which requires one or more of the same
