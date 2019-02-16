440837170f02e28b2b7302e0a2507cc2e48a25df
FYREStation <fyre5480@gmail.com>
2019-02-15 21:28:26 -0800

    changed value o

--- deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftUpCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftUpCommand.java
@@ -26,7 +26,7 @@ public class ManualLiftUpCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.liftSubsystemRef.lift(0.25);
+    Robot.liftSubsystemRef.lift(0.5);
   }
 
   // Make this return true when this Command no longer needs to run execute()
