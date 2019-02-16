2c2162e07fb96144f0e39f9a650610dbfd8bef11
Michael Hayashi <mikephayashi@gmail.com>
2019-02-15 19:50:27 -0800

    try one command execute speed 1, end speed 0

--- deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftUpCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Lift/ManualLiftUpCommand.java
@@ -32,13 +32,13 @@ public class ManualLiftUpCommand extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return true;
+    return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    
+    Robot.liftSubsystemRef.lift(0);
   }
 
   // Called when another command which requires one or more of the same
