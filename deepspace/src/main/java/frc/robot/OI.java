d75e9b9c87ace98c4820b89b9f98b1e65622929b
FYREStation <fyre5480@gmail.com>
2019-02-16 13:49:09 -0800

    commented out a and b buttons

--- deepspace/src/main/java/frc/robot/OI.java
+++ deepspace/src/main/java/frc/robot/OI.java
@@ -74,14 +74,14 @@ public class OI {
     button5.whileHeld(new ExtendRackAndPinionCommand());
     //button  6 goes forward
     button6.whileHeld(new RetractRackAndPinionCommand());
-    //button 8 forwards
-    button8.whileHeld(new ManualLiftUpCommand());
-    //button 10 backwards
-    button10.whileHeld(new ManualLiftDownCommand());
+    //button 8 backwards
+    button10.whileHeld(new ManualLiftUpCommand());
+    //button 10 forwards
+    button8.whileHeld(new ManualLiftDownCommand());
     // button a is engaged
-    buttonA.whenPressed(new DisengageBreakCommand());
+    //buttonA.whenPressed(new DisengageBreakCommand());
     //button b disengaged
-    buttonB.whenPressed(new EngageBreakCommand());
+    //buttonB.whenPressed(new EngageBreakCommand());
 
   }
 
