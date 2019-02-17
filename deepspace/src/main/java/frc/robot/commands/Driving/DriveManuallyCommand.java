6aa499c236a0cb07bec2174df643ae15188e9d16
Parminder <parmarp1975@gmail.com>
2019-02-17 13:59:11 -0800

    changed joystick

--- deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
+++ deepspace/src/main/java/frc/robot/commands/Driving/DriveManuallyCommand.java
@@ -32,7 +32,7 @@ public class DriveManuallyCommand extends Command {
   protected void execute() {
     
     double move = Robot.m_oi.m_xBox.getY(Hand.kLeft);
-    double turn = Robot.m_oi.m_xBox.getX(Hand.kLeft);
+    double turn = Robot.m_oi.m_xBox.getX(Hand.kRight);
     Robot.driveSubsystemRef.manualDrive(move, turn);
   }
 
